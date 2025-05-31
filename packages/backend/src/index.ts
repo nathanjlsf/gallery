70da7921476b823055bab35772be1425174d2a22
Nathan Lee <nathan.lee5459@gmail.com>
2025-05-30 19:32:03 -0700

    backend:

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { ValidRoutes } from "shared/ValidRoutes";
+import { ValidRoutes } from "./shared/ValidRoutes";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
