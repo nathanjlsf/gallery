dc10b3ee05cc9b2b6d7fed2475479c57a3c2acc8
Nathan Lee <nathan.lee5459@gmail.com>
2025-05-30 19:02:54 -0700

    lab 19

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,25 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { ValidRoutes } from "shared/ValidRoutes";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
 
+const STATIC_DIR = process.env.STATIC_DIR || "public";
+
 const app = express();
+
+app.use(express.static(STATIC_DIR));
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
+});
+
+Object.values(ValidRoutes).forEach((routePath) => {
+  app.get(routePath, (req, res) => {
+    res.sendFile(path.join(__dirname, STATIC_DIR, "index.html"));
+  });
 });
 
 app.listen(PORT, () => {
